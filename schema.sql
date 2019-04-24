d9cee7dbf3165766e2f09f1510fb7899ea26a210
Sabrina Li <sabrinali0312@gmail.com>
2019-04-24 11:15:13 -0400

    modify schema with foreign key constraint and seed script

--- schema.sql
+++ schema.sql
@@ -1,6 +1,9 @@
+DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE IF NOT EXISTS bamazon;
 
 USE bamazon;
+
+
 
 CREATE TABLE IF NOT EXISTS products(
 item_id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
@@ -17,4 +20,9 @@ over_head_costs INT NOT NULL
 );
 
 ALTER TABLE products
-ADD COLUMN product_salesE INT NOT NULL;
+ADD COLUMN product_sales FLOAT NOT NULL DEFAULT 0,
+CHANGE COLUMN department_name department_id INT,
+ADD CONSTRAINT FK_DepartmentID
+FOREIGN KEY (department_id) REFERENCES departments(department_id);
+
+
--- seed.sql
+++ seed.sql
@@ -1,17 +1,24 @@
+INSERT INTO departments(department_name,over_head_costs)
+VALUES ('clothing',27.59),
+		('pets',20),
+		('book',35),
+		('furniture',56.24),
+		('electronics',569.35);
 
--- INSERT INTO products(product_name,department_name,price,stock_quantity)
--- VALUES ('shorts','clothing',27.59,100),
--- ('doggo','pets',500.00,6),
--- ('harry potter','book',100.00,5),
--- ('couch','furniture',1500.00,4),
--- ('TV','furniture',500.00,3),
--- ('monitor','electronics',750.50,2),
--- ('keyboards','electronics',20.00,5)
--- ('macbook','electronics',2500.00,20),
--- ('shoes','clothing',100.00,50),
--- ('t-shirt','clothing',10.00,150)
+INSERT INTO products(product_name,department_id,price,stock_quantity)
+VALUES ('shorts',1,27.59,100),
+		('doggo',2,500.00,6),
+		('harry potter',3,100.00,5),
+		('couch',4,1500.00,4),
+		('TV',4,500.00,3),
+		('monitor',5,750.50,2),
+		('keyboards',5,20.00,5),
+		('macbook',5,2500.00,20),
+		('shoes',1,100.00,50),
+		('t-shirt',1,10.00,150);
 
 
-select * from products
+SELECT * FROM products;
+SELECT * FROM departments;
 
 
