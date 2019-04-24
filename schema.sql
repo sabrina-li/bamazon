c7f315c32fe3656c6b20d5207a526aa351ed8f07
Sabrina Li <sabrinali0312@gmail.com>
2019-04-24 10:57:36 -0400

    modify schema to add product sales

--- schema.sql
+++ schema.sql
@@ -8,14 +8,13 @@ product_name VARCHAR(200) NOT NULL,
 department_name VARCHAR(100),
 price DECIMAL(8,2),
 stock_quantity INT NOT NULL
-)
+);
 
-INSERT INTO products(product_name,department_name,price,stock_quantity)
-VALUES ('macbook','electronics',2500.00,20)
+CREATE TABLE IF NOT EXISTS departments(
+department_id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(100) NOT NULL,
+over_head_costs INT NOT NULL
+);
 
-select * from products
-LIMIT 1
-
-
-
-
+ALTER TABLE products
+ADD COLUMN product_salesE INT NOT NULL;
